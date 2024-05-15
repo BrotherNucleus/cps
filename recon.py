540992069fc8ac40c2c132eda5b17a2be4861644
BrotherNucleus <115566971+BrotherNucleus@users.noreply.github.com>
2024-05-15 10:06:59 +0200

    sinc works
    KURWAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA DZIAŁAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- recon.py
+++ recon.py
@@ -3,8 +3,9 @@ import wave as w
 import math
 import matplotlib.pyplot as plt
 
+N = 5
+
 def reconstruct(res, type):
-    N = 1
     xDiff = res[1][0] - res[0][0]
     xJump = xDiff/N
     l = (len(res[:, [1]])-1)*N + 1
@@ -34,35 +35,30 @@ def reconstruct(res, type):
             zeros[z] = (1 / r)*z
             zeros[z + r] = 1 - (1/r)*z           
     elif type == 3:
-        Len = round(x[len(x) - 1])
+        Len = round(x[len(x) - 1])*1000
         L = Len / len(res[:, [1]])
         zeros=np.zeros(Len,float)
-        v = np.zeros(Len, float)
-        x = np.zeros(Len, float)
-        for i in range(len(res[:, [1]]) - 1):
-            v[i*L] = res[i][1]
-            x[i*L] = res[i][0]
-        v[len(v)-1] = res[len(res[:, [1]]) - 1][1]
-        x[len(x)-1] = res[len(res[:, [1]]) - 1][0]
-        #print(v)
-        for i in range(len(x)):
-            if i == 0:
-                continue
-            else:
-                x[i] = x[i-1] + L
         for i in range(0,Len):
             t=(Len//2-i)/L
             if (t==0):
                 z=1
             else:
                 z=math.sin(np.pi*t)/(np.pi*t)
-                print(z)
+                #print(z)
             zeros[i]=z
+        rl = round(len(zeros) / l) 
+        tres = []
+        for k in range(l):
+            tres.append(zeros[k*rl])
+        zeros = tres
     result = np.convolve(v, zeros, 'same')
-    print(len(zeros))
-    #result = zeros
     plt.plot(zeros)
     plt.show()
+    print(len(result))
+    #print(len(zeros))
+    #result = zeros
+    # plt.plot(result)
+    # plt.show()
     end = np.zeros((len(result), 2))
     # x = np.linspace(0, xDiff, Len)
     for i in range(0, len(result)):
